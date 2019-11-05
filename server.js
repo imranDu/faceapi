a27b930af53f5c91a65779d311cf036e865a3414
User <imranbaganchara@gmail.com>
2019-11-05 19:25:39 +0600

    origin fixing

--- server.js
+++ server.js
@@ -18,9 +18,22 @@ const db = knex({
 const app = express();
 
 app.use(bodyParser.json());
+var allowedOrigins = ['http://localhost:3000',
+                      'https://git.heroku.com/face-s.git'];
 app.use(cors({
-  origin: 'https://git.heroku.com/face-s.git'
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
+
 app.get('/',(req,res)=>{
 	res.send("db.users");
 })
